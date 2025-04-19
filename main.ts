f350bf9a91bc0534a63215d21baf06de5c44c514
Phatisena <naphatism@hotmail.com>
2025-04-19 12:46:49 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -19,7 +19,15 @@ namespace DataBuffer {
     //% weight=100
     export function encodeText(txtv: string) {
         let numarrv: number[] = []
-        for (let i = 0; i < txtv.length; i++) { let numv = txtv.charCodeAt(i), bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4; numarrv.push(bytelen); for (let j = 0; j < bytelen; j++) numarrv.push(numv % bytemax), numv = Math.floor(numv / bytemax) } numarrv.push(0)
+        for (let i = 0; i < txtv.length; i++) {
+            let numv = txtv.charCodeAt(i), bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4
+            numarrv.push(bytelen)
+            for (let j = 0; j < bytelen; j++) {
+                numarrv.push(numv % bytemax)
+                numv = Math.floor(numv / bytemax)
+            }
+        }
+        numarrv.push(0)
         return pins.createBufferFromArray(numarrv)
     }
 
@@ -30,12 +38,21 @@ namespace DataBuffer {
      */
     //% blockid=databuff_decode_string
     //% block="get buffer of $bufv convert to string"
-    //% bufv.shadow=variables_get bufv.defl=Buffer
+    //% bufv.shadow=variables_get bufv.defl=buffer
     //% group="string"
     //% weight=75
     export function decodeText(bufv: Buffer) {
         let strtxt: string = "", bytelen = bufv[0], bytesum = 0, byteval = 0, bytemax = 8 ** 2 * 4
-        for (let i = 1; i < bufv.length; i++) if (bytelen > 0) { if (bytesum > 0) byteval += bufv[i] * bytesum; else byteval += bufv[i]; bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, bytelen-- } else { bytelen = bufv[i], strtxt += String.fromCharCode(byteval), byteval = 0, bytesum = 0 }
+        for (let i = 1; i < bufv.length; i++) {
+            if (bytelen > 0) {
+                if (bytesum > 0) byteval += bufv[i] * bytesum
+                else byteval += bufv[i]
+                bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, bytelen--
+            } else {
+                bytelen = bufv[i], strtxt += String.fromCharCode(byteval)
+                byteval = 0, bytesum = 0
+            }
+        }
         return strtxt
     }
 
@@ -50,7 +67,16 @@ namespace DataBuffer {
     //% weight=50
     export function encodeTextArr(txtarr: string[]) {
         let numarrv: number[] = []
-        for (let txtv of txtarr) { for (let i = 0; i < txtv.length; i++) { let numv = txtv.charCodeAt(i), bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4; numarrv.push(bytelen); for (let j = 0; j < bytelen; j++) { numarrv.push(numv % bytemax), numv = Math.floor(numv / bytemax) } } numarrv.push(0) }
+        for (let txtv of txtarr) {
+            for (let i = 0; i < txtv.length; i++) {
+                let numv = txtv.charCodeAt(i), bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4
+                numarrv.push(bytelen)
+                for (let j = 0; j < bytelen; j++) {
+                    numarrv.push(numv % bytemax)
+                    numv = Math.floor(numv / bytemax)
+                }
+            } numarrv.push(0)
+        }
         return pins.createBufferFromArray(numarrv)
     }
 
@@ -61,12 +87,25 @@ namespace DataBuffer {
      */
     //% blockid=databuff_decode_string_array
     //% block="get buffer of $bufv convert to string array"
-    //% bufv.shadow=variables_get bufv.defl=Buffer
+    //% bufv.shadow=variables_get bufv.defl=buffer
     //% group="string"
     //% weight=25
     export function decodeTextArr(bufv: Buffer) {
         let strarr: string[] = [], strtxt: string = "", bytelen = bufv[0], bytesum = 0, byteval = 0, bytemax = 8 ** 2 * 4
-        for (let i = 1; i < bufv.length; i++) if (bytelen > 0) { if (bytesum > 0) { byteval += bufv[i] * bytesum } else { byteval += bufv[i] } bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, bytelen-- } else if (bytelen <= 0 && bytelen == bufv[i]) { strtxt += String.fromCharCode(byteval), strarr.push(strtxt), strtxt = "" } else { bytelen = bufv[i], strtxt += String.fromCharCode(byteval), byteval = 0, bytesum = 0 }
+        for (let i = 1; i < bufv.length; i++) {
+            if (bytelen > 0) {
+                if (bytesum > 0) byteval += bufv[i] * bytesum
+                else byteval += bufv[i] 
+                bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+                bytelen--
+            } else if (bytelen <= 0 && bytelen == bufv[i]) {
+                strtxt += String.fromCharCode(byteval)
+                strarr.push(strtxt), strtxt = ""
+            } else {
+                bytelen = bufv[i], strtxt += String.fromCharCode(byteval)
+                byteval = 0, bytesum = 0
+            }
+        }
         return strarr
     }
 
@@ -81,12 +120,46 @@ namespace DataBuffer {
     //% weight=100
     export function encodeImage(img: Image) {
         let numarrv: number[] = [], bytelen: number,bytemax: number, numv: number, numc: number
-        numv = img.width, bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4, numarrv.push(bytelen)
-        for (let j = 0; j < bytelen; j++) numarrv.push(numv % bytemax), numv = Math.floor(numv / bytemax)
-        numv = img.height, bytelen = bitcalc(numv, 8), bytemax = 8 ** 2 * 4, numarrv.push(bytelen)
-        for (let j = 0; j < bytelen; j++) numarrv.push(numv % bytemax), numv = Math.floor(numv / bytemax)
+        numv = img.width
+        bytelen = bitcalc(numv, 8)
+        bytemax = 8 ** 2 * 4
+        numarrv.push(bytelen)
+        for (let j = 0; j < bytelen; j++) {
+            numarrv.push(numv % bytemax)
+            numv = Math.floor(numv / bytemax)
+        }
+        numv = img.height
+        bytelen = bitcalc(numv, 8)
+        bytemax = 8 ** 2 * 4
+        numarrv.push(bytelen)
+        for (let j = 0; j < bytelen; j++) {
+            numarrv.push(numv % bytemax)
+            numv = Math.floor(numv / bytemax) 
+        }
         numv = img.getPixel(0, 0), numc = 1
-        for (let i = 1;i < (img.width * img.height);i++) { const xi = Math.floor(i / img.height), yi = (i % img.height); if (img.getPixel(xi, yi) == numv) { numc++ } else { numarrv.push(numc), numarrv.push(numv), numv = img.getPixel(xi, yi), numc = 1 } }
+        for (let i = 1;i < (img.width * img.height);i++) {
+            const xi = Math.floor(i / img.height), yi = (i % img.height)
+            if (img.getPixel(xi, yi) == numv) {
+                numc++
+            } else { 
+                bytelen = bitcalc(numc, 8), numarrv.push(bytelen)
+                while (bytelen > 0) {
+                    numarrv.push(numc % bytemax)
+                    numc = Math.floor(numc / bytemax)
+                    bytelen--
+                }
+                numarrv.push(numv)
+                numv = img.getPixel(xi, yi)
+                numc = 1
+            }
+        }
+        bytelen = bitcalc(numc, 8), numarrv.push(bytelen)
+        while (bytelen > 0) {
+            numarrv.push(numc % bytemax)
+            numc = Math.floor(numc / bytemax)
+            bytelen--
+        }
+        numarrv.push(numv)
         return pins.createBufferFromArray(numarrv)
     }
 
@@ -97,16 +170,60 @@ namespace DataBuffer {
      */
     //% blockid=databuff_decode_image
     //% block="get buffer of $bufv convert to image"
-    //% bufv.shadow=variables_get bufv.defl=Buffer
+    //% bufv.shadow=variables_get bufv.defl=buffer
     //% group="image"
     //% weight=50
     export function decodeImage(bufv: Buffer) {
-        let i = 0, i0 = 0, bytemax = 8 ** 2 * 4, bytelen = bufv[i], byteval = 0, bytesum = 0, w = 0, h = 0, img: Image, imgRowBuffer: Buffer
-        while (bytelen > 0) { i++; if (bytesum > 0) byteval += bufv[i] * bytesum; else byteval += bufv[i]; bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, bytelen-- } i++, w = byteval, bytelen = bufv[i], byteval = 0, bytesum = 0
-        while (bytelen > 0) { i++; if (bytesum > 0) byteval += bufv[i] * bytesum; else byteval += bufv[i]; bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, bytelen-- } i++, h = byteval, img = image.create(w, h), imgRowBuffer = pins.createBuffer(h), bytelen = bufv[i], i++, byteval = bufv[i], i++
-        while (i < bufv.length) { for (let i1 = 0;i1 < h;i1++) { if (bytelen > 0) { imgRowBuffer[i1] = bufv[i] } else { bytelen = bufv[i], i++, byteval = bufv[i], i++ } } img.setRows(i0, imgRowBuffer), i0++}
+        let i = 0, ii = 0, bytemax = 8 ** 2 * 4, bytelen = bufv[i], byteval = 0, bytesum = 0, w = 0, h = 0, img: Image, imgRowBuffer: Buffer
+        while (bytelen > 0) {
+            i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen--
+        } i++, w = byteval, bytelen = bufv[i], byteval = 0, bytesum = 0
+        while (bytelen > 0) {
+            i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen-- 
+        } i++, h = byteval, img = image.create(w, h), imgRowBuffer = pins.createBuffer(h)
+        bytelen = bufv[i], byteval = 0, bytesum = 0
+        while (bytelen > 0) { i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen--
+        } i++, bytelen = byteval
+        byteval = bufv[i], i++
+        while (i < bufv.length) {
+            const xi = Math.floor(ii / h), yi = (ii % h)
+                if (bytelen > 0) {
+                    img.setPixel(xi,yi,byteval)
+                    bytelen--
+                }
+                if (bytelen <= 0) {
+                    bytelen = bufv[i]
+                    while (bytelen > 0) { i++
+                        if (bytesum > 0) byteval += bufv[i] * bytesum
+                        else byteval += bufv[i]
+                        bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+                        bytelen--
+                    } i++, bytelen = byteval
+                    byteval = bufv[i]
+                }
+        ii++ }
         return img
     }
 
 }
 
+let mySprite = sprites.create(img`
+    2 3 3
+    2 3 3
+    2 3 3
+`, SpriteKind.Player)
+let Buffer2 = DataBuffer.encodeImage(mySprite.image)
+mySprite.setImage(DataBuffer.decodeImage(Buffer2))
+scene.setBackgroundColor(1)
