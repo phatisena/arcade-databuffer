578a68f0ef4c8551f2302d14e39603057101464d
Phatisena <naphatism@hotmail.com>
2025-04-20 14:17:53 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -194,7 +194,7 @@ namespace DataBuffer {
      */
     //% blockid=databuff_encode_image_array
     //% block="get array image of $imgarr convert to buffer"
-    //% imgarr.shadow="lists_create_with" imgarr.defl=screen_image_picker
+    //% imgarr.shadow=lists_create_with imgarr.defl=screen_image_picker
     //% group="image"
     //% weight=50
     export function encodeImageArray(imgarr: Image[]) {
@@ -222,6 +222,7 @@ namespace DataBuffer {
             bytelen = Math.max(bitcalc(numc), 1), numarrv.push(bytelen)
             while (numc > 0) { numarrv.push(numc % bytemax)
             numc = Math.floor(numc / bytemax), bytelen-- } numarrv.push(numv)
+            numarrv.push(0)
         }
         return pins.createBufferFromArray(numarrv)
     }
@@ -271,43 +272,13 @@ namespace DataBuffer {
                 }
                 bytelen = byteval, byteval = bufv[i], i++
             }
-            if ((yi % h)+1 == h) img.setRows(xi, imgRowBuffer)
+            if ((yi % h)+1 == h) { img.setRows(xi, imgRowBuffer) }
+        } i--, i--
+        imgarr.push(img.clone())
+        bytelen = bufv[i], byteval = 0, bytesum = 0, w = 0, h = 0
         }
-        if (bufv[i] == 0) { imgarr.push(img.clone())
-        i++, bytelen = bufv[i]}
-        }
-        return img
+        return imgarr
     }
     
 }
-
-let mySprite = sprites.create(img`
-    ...............bbbbbbbbbbbbbbbbbbb...............
-    ...........bbbbdd111111111111111ddbbbb...........
-    ........bbbd1111111111111111111111111dbbb........
-    ......bbd11111111dddddddddddddd111111111dbb......
-    ....bbd1111111ddd11111111111111dddd1111111dbb....
-    ...bd111111ddd111111111111111111111ddd111111db...
-    ..bd11111ddd111ddddddddddddddddddd111ddd11111db..
-    .bd11111dd111dddd111111111111111dddd111dd11111db.
-    .b11111d111ddd111111111111111111111ddd111d11111b.
-    bd11111d1ddd1111111111111111111111111ddd1111111db
-    b11111d1ddd111111111111111111111111111ddd1d11111b
-    b11111ddddd111111111111111111111111111ddddd11111b
-    b11111ddddd111111111111111111111111111dddbd11111b
-    b111111dddd111111111111111111111111111dddb111111b
-    bd111111dddd1111111111111111111111111dddbd11111db
-    .b1111111dddd11111111111111111111111dddbd111111b.
-    .bd1111111dbbdd1111111111111111111dddbbd111111db.
-    ..bd11111111dbbdd111111111111111dddbbd1111111db..
-    ...bd111111111dbbbbbbdddddddddddddd111111111db...
-    ....bbd11111111111dbbbbbbbbbddd11111111111dbb....
-    ......bbdd11111111111111111111111111111ddbb......
-    ........bbbdd11111111111111111111111ddbbb........
-    ...........bbbbbddd11111111111dddbbbbb...........
-    ................bbbbbbbbbbbbbbbbb................
-`, SpriteKind.Player)
-let Buffer2 = DataBuffer.encodeImage(mySprite.image)
-mySprite.setImage(DataBuffer.decodeImage(Buffer2))
-scene.setBackgroundColor(1)
 
