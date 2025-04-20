2f12b3e33fdee5ed5691b351e7de23b8e685b0f9
Phatisena <naphatism@hotmail.com>
2025-04-20 13:28:55 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -63,7 +63,7 @@ namespace DataBuffer {
     //% block="get string array of $txtarr convert to buffer"
     //% group="string"
     //% weight=50
-    export function encodeTextArr(txtarr: string[]) {
+    export function encodeTextArray(txtarr: string[]) {
         let numarrv: number[] = []
         for (let txtv of txtarr) {
             for (let i = 0; i < txtv.length; i++) {
@@ -86,7 +86,7 @@ namespace DataBuffer {
     //% bufv.shadow=variables_get bufv.defl=buffer
     //% group="string"
     //% weight=25
-    export function decodeTextArr(bufv: Buffer) {
+    export function decodeTextArray(bufv: Buffer) {
         let strarr: string[] = [], strtxt: string = "", bytelen = bufv[0], bytesum = 0, byteval = 0
         for (let i = 1; i < bufv.length; i++) {
             if (bytelen > 0) {
@@ -147,7 +147,7 @@ namespace DataBuffer {
     //% block="get buffer of $bufv convert to image"
     //% bufv.shadow=variables_get bufv.defl=buffer
     //% group="image"
-    //% weight=50
+    //% weight=75
     export function decodeImage(bufv: Buffer) {
         let i = 0, bytelen = bufv[i], byteval = 0, bytesum = 0, w = 0, h = 0, img: Image, imgRowBuffer: Buffer
         while (bytelen > 0) { i++
@@ -187,6 +187,98 @@ namespace DataBuffer {
         return img
     }
 
+    /**
+     * convert array of image to buffer string
+     * @param array image to encode
+     * @returns after convert array image to buffer string
+     */
+    //% blockid=databuff_encode_image_array
+    //% block="get array image of $imgarr convert to buffer"
+    //% imgarr.shadow="lists_create_with" imgarr.defl=screen_image_picker
+    //% group="image"
+    //% weight=50
+    export function encodeImageArray(imgarr: Image[]) {
+        let numarrv: number[] = [], bytelen: number, numv: number, numc: number
+        for (let img of imgarr) {
+            numv = img.width, bytelen = bitcalc(numv), numarrv.push(bytelen)
+            while (numv > 0) { numarrv.push(numv % bytemax)
+            numv = Math.floor(numv / bytemax) }
+            numv = img.height, bytelen = bitcalc(numv), numarrv.push(bytelen)
+            while (numv > 0) { numarrv.push(numv % bytemax)
+            numv = Math.floor(numv / bytemax) }
+            numv = img.getPixel(0, 0), numc = 1
+            for (let i = 1;i < (img.width * img.height);i++) {
+                const xi = Math.floor(i / img.height), yi = (i % img.height)
+                if (img.getPixel(xi, yi) == numv) {
+                    numc++
+                } else {
+                    bytelen = Math.max(bitcalc(numc), 1), numarrv.push(bytelen)
+                    while (numc > 0) { numarrv.push(numc % bytemax)
+                    numc = Math.floor(numc / bytemax)
+                    bytelen-- } numarrv.push(numv)
+                    numv = img.getPixel(xi, yi), numc = 1
+                }
+            }
+            bytelen = Math.max(bitcalc(numc), 1), numarrv.push(bytelen)
+            while (numc > 0) { numarrv.push(numc % bytemax)
+            numc = Math.floor(numc / bytemax), bytelen-- } numarrv.push(numv)
+        }
+        return pins.createBufferFromArray(numarrv)
+    }
+
+    /**
+     * convert buffer string to array image
+     * @param buffer string to decode
+     * @returns after convert buffer string to array image
+     */
+    //% blockid=databuff_decode_image_array
+    //% block="get buffer of $bufv convert to array image"
+    //% bufv.shadow=variables_get bufv.defl=buffer
+    //% group="image"
+    //% weight=25
+    export function decodeImageArray(bufv: Buffer) {
+        let i = 0, imgarr: Image[] = [], bytelen = bufv[i], byteval = 0, bytesum = 0, w = 0, h = 0, img: Image, imgRowBuffer: Buffer
+        while (i < bufv.length) {
+        while (bytelen > 0) { i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen--
+        } i++, w = byteval, bytelen = bufv[i], byteval = 0, bytesum = 0
+        while (bytelen > 0) { i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen-- }
+        i++, h = byteval, img = image.create(w, h), imgRowBuffer = pins.createBuffer(h), bytelen = bufv[i], byteval = 0, bytesum = 0
+        while (bytelen > 0) { i++
+            if (bytesum > 0) byteval += bufv[i] * bytesum
+            else byteval += bufv[i]
+            bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax
+            bytelen-- } i++, bytelen = byteval, byteval = bufv[i], i++
+        for (let ii = 0;ii < (w * h);ii++) {
+            const xi = Math.floor(ii / h), yi = (ii % h)
+            if (bytelen > 0) {
+                imgRowBuffer[yi] = byteval
+                bytelen--
+            }
+            if (bytelen <= 0 && i < bufv.length) {
+                bytelen = Math.max(bufv[i], 1), byteval = 0, bytesum = 0, i++
+                while (bytelen > 0) { bytelen-- 
+                    if (bytesum > 0) byteval += bufv[i] * bytesum
+                    else byteval += bufv[i]
+                    bytesum = (bytesum > 0) ? bytesum * bytemax : bytemax, i++
+                }
+                bytelen = byteval, byteval = bufv[i], i++
+            }
+            if ((yi % h)+1 == h) img.setRows(xi, imgRowBuffer)
+        }
+        if (bufv[i] == 0) { imgarr.push(img.clone())
+        i++, bytelen = bufv[i]}
+        }
+        return img
+    }
+    
 }
 
 let mySprite = sprites.create(img`
